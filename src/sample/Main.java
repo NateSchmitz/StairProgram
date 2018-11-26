8fff43d39673247d90b4cf54793a35b52e3edc37
Nate <nateschmitz17@gmail.com>
2018-11-26 16:26:27 -0600

    Second Push

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,15 +3,17 @@ package sample;
 import javax.swing.*;
 
 public class Main {
-    int N;
-    public static int num_ways(){
-
-        return 100;
+    public static int num_ways(int N){
+        num_ways(1);
+        return 1;
+        num_ways(0);
+        return 0;
         }
 
     public static void main(String[] args) {
         String numString = JOptionPane.showInputDialog(null,"How many stairs are you climbing?", "Input", JOptionPane.PLAIN_MESSAGE);
-        System.out.println(numString);
+        int numInt = Integer.parseInt(numString);
+        System.out.println(numInt);
         System.out.println(num_ways());
     }
 }
